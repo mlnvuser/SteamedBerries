37220ec97769dee52e457e3084159aaa6b5b4e91
mlnvuser <thebrilliantplayer@gmail.com>
2025-06-16 21:55:36 +0300

    create app orders

--- users/views.py
+++ users/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, redirect
+from django.contrib import auth, messages
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .forms import UserLoginForm, UserRegistrationForm, ProfileForm
+from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,
+                                     password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:product_list'))
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login.html', {'form': form})
 
 def registration(request):
     return render(request, 'users/registration.html')
