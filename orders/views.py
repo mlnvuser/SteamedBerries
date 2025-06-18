ec57e005b5883203bbfb18b6df5e04f39bb6de7f
mlnvuser <thebrilliantplayer@gmail.com>
2025-06-18 22:16:03 +0300

    work with orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST, request=request)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                discounted_price = item['product'].sell_price()
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=discounted_price,
+                                         quantity=item['quantity'])
+            cart.clear()
+            return render(request,
+                          'order/created.html',
+                          {'order': order,
+                           'form': form})
+    else:
+        form = OrderCreateForm(request=request)
+    return render(request,
+                  'order/create.html',
+                  {'cart': cart,
+                   'form': form})
+
